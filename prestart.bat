be1bd2b3253f81e0029f248f1c846273ac3bd853
deniz@armut.com <deniz@armut.com>
2018-01-03 18:49:45 +0300

    prestart.bat updates

--- prestart.bat
+++ prestart.bat
@@ -1,2 +1,3 @@
-call  emcc -Oz .\native\fibonacci.c -s WASM=1 -s SIDE_MODULE=1 -o .\public\fibonacci.wasm
+call emcc -Oz .\native\fibonacci.c -s WASM=1 -s SIDE_MODULE=1 -o .\public\fibonacci.wasm
+call emcc -Oz .\native\factorial.c -s WASM=1 -s SIDE_MODULE=1 -o .\public\factorial.wasm
 call node towast.js
